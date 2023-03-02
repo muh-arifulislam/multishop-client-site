478ae1ca17b4a61917e9ee555272f9a33fafd47b
muh-arifulislam <arifibnenam@gmail.com>
2023-03-03 02:18:24 +0600

    feature section addded

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Categories from "./Categories";
+import Features from "./Features";
 import HomeCarousel from "./HomeCarousel";
 
 const Home = () => {
   return (
     <div>
       <HomeCarousel></HomeCarousel>
+      <Features></Features>
+      <Categories></Categories>
     </div>
   );
 };
